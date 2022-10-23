437fab6adc89fa4fd61faf3080470e581af35987
miodine <rosenblutwurst@gmail.com>
2022-10-23 20:19:01 +0200

    packaging

--- treco/core.py
+++ treco/core.py
@@ -4,51 +4,48 @@ from bs4 import BeautifulSoup
 import json
 
 
-HEADERS = {
-    'User-Agent': 'TrecoBot/1.0'}
-MAIN_URL = 'https://tradingeconomics.com/'
+HEADERS = {"User-Agent": "TrecoBot/1.0"}
+MAIN_URL = "https://tradingeconomics.com/"
 
 
 def __format_dataframe(dataframe: pd.DataFrame, fmt: str):
     fmt = fmt.lower()
 
-    if fmt == 'json':
+    if fmt == "json":
         return dataframe.to_json()
-    elif fmt == 'csv':
+    elif fmt == "csv":
         return dataframe.to_csv()
-    elif fmt == 'dict':
+    elif fmt == "dict":
         return dataframe.to_dict()
-    elif fmt == 'html':
+    elif fmt == "html":
         return dataframe.to_html()
-    elif fmt == 'numpy' or 'np':
+    elif fmt == "numpy" or "np":
         return dataframe.to_numpy()
-    elif fmt == 'pickle':
+    elif fmt == "pickle":
         return dataframe.to_pickle()
-    elif fmt == 'latex':
+    elif fmt == "latex":
         return dataframe.to_latex()
     else:
         return dataframe
 
 
-def get_data_from_stream(begin=0, size=1, country='NULL', category='NULL') -> list:
+def get_data_from_stream(begin=0, size=1, country="NULL", category="NULL") -> list:
     headers = HEADERS
-    stream_url = 'https://tradingeconomics.com/ws/stream.ashx'
+    stream_url = "https://tradingeconomics.com/ws/stream.ashx"
 
-    stream_get_args = '?start={begin}&size={size}'.format(
-        begin=begin, size=size)
-    if country != 'NULL':
-        stream_get_args += 'c={}'.format(country.lower().replace(' ', '+'))
-    if country != 'NULL':
-        stream_get_args += 'i={}'.format(category.lower().replace(' ', '+'))
+    stream_get_args = "?start={begin}&size={size}".format(begin=begin, size=size)
+    if country != "NULL":
+        stream_get_args += "c={}".format(country.lower().replace(" ", "+"))
+    if country != "NULL":
+        stream_get_args += "i={}".format(category.lower().replace(" ", "+"))
 
-    url = stream_url+stream_get_args
+    url = stream_url + stream_get_args
 
     resp = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(resp.content, 'html.parser',
-                         from_encoding='utf-8')
+    soup = BeautifulSoup(resp.content, "html.parser", from_encoding="utf-8")
 
-    result_json = json.loads(soup.text.encode('utf-8', 'ignore'))
+    result_json = json.loads(soup.text.encode("utf-8", "ignore"))
 
     return result_json
 
@@ -57,9 +54,9 @@ def get_data_from_tables(url: str) -> list:
     headers = HEADERS
 
     response = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    tables = soup.find_all('table', 'table')
+    tables = soup.find_all("table", "table")
 
     df_tables_list = []
 
@@ -82,22 +79,22 @@ def scrape_specific_data(server_path, format):
 def get_data_from_lists(url: str, as_list=False):
     headers = HEADERS
     response = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    lists = soup.find_all('ul', {'class': 'list-unstyled'})
+    lists = soup.find_all("ul", {"class": "list-unstyled"})
 
     df_return = {}
 
     for list_ in lists:
 
-        list_contents = list_.find_all('li')
+        list_contents = list_.find_all("li")
         df_list_contents = {}
         key = ""
 
         for list_item in list_contents:
-            a = list_item.find('a')
+            a = list_item.find("a")
             if a is not None:
-                df_list_contents[list_item.text] = a.get('href')
+                df_list_contents[list_item.text] = a.get("href")
             else:
                 key = list_item.text
         if as_list == True:
--- treco/extras.py
+++ treco/extras.py
@@ -3,22 +3,22 @@
 # 2. Heatmap
 # 3. News
 
-from core import HEADERS, MAIN_URL, get_data_from_tables
-from core import get_data_from_stream
+from treco.core import HEADERS, MAIN_URL, get_data_from_tables
+from treco.core import get_data_from_stream
 
 
-def news(amount=10, country='NULL', category='NULL'):
+def news(amount=10, country="NULL", category="NULL"):
     return get_data_from_stream(size=amount, country=country, category=category)
 
 
 def heatmap(region=""):
-    get_args = '?g=' + region.lower()
-    url = MAIN_URL+"matrix"+get_args
+    get_args = "?g=" + region.lower()
+    url = MAIN_URL + "matrix" + get_args
     ret_val = get_data_from_tables(url)
     return ret_val
 
 
 def _experimental_calendar():
-    url = MAIN_URL+"calendar"
+    url = MAIN_URL + "calendar"
     ret_val = get_data_from_tables(url)
     return ret_val
